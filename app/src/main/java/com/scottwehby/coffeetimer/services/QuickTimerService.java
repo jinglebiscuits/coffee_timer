4495d53be16ea8ab7819e48fbdc2c38180cc4b5b
jinglebiscuits <scott.wehby@gmail.com>
2018-05-18 06:53:35 -0500

    init

---
+++ app/src/main/java/com/scottwehby/coffeetimer/services/QuickTimerService.java
@@ -0,0 +1,61 @@
+package com.scottwehby.coffeetimer.services;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.provider.AlarmClock;
+import android.service.quicksettings.TileService;
+import android.util.Log;
+
+/**
+ Created by scottwehby on 9/10/17.
+ */
+
+public class QuickTimerService extends TileService {
+
+    public QuickTimerService() {
+        super();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        Log.d(this.getClass().getSimpleName(), "Start the timer");
+        super.onClick();
+        Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER)
+          .putExtra(AlarmClock.EXTRA_LENGTH, 240)
+          .putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+          .putExtra(AlarmClock.EXTRA_MESSAGE, "Coffee Time");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+}
