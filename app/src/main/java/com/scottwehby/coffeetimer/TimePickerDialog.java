a181405efabbfe7003b0724281c06e8494c6b994
Scott Wehby <scott.wehby@helplightning.com>
2019-03-09 12:28:28 -0600

    storing user input

--- app/src/main/java/com/scottwehby/coffeetimer/TimePickerDialog.java
+++ app/src/main/java/com/scottwehby/coffeetimer/TimePickerDialog.java
@@ -1,33 +1,19 @@
 package com.scottwehby.coffeetimer;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.preference.DialogPreference;
 import androidx.preference.PreferenceDialogFragmentCompat;
 
 public class TimePickerDialog extends PreferenceDialogFragmentCompat {
 
+    private EditText minuteDigit1, minuteDigit2, secondDigit1, secondDigit2;
     private int minutes;
     private int seconds;
-
-//    protected TimePickerDialog(@NonNull Context context, int minutes, int seconds) {
-//        super(context);
-//        this.minutes = minutes;
-//        this.seconds = seconds;
-//
-//        setDialogLayoutResource(R.layout.timer_length_preference);
-//    }
-//
-//    public TimePickerDialog(Context context, AttributeSet attrs) {
-//        super(context, attrs);
-////        setDialogLayoutResource(R.layout.timer_length_preference);
-//    }
 
     @NonNull
     @Override
@@ -37,13 +23,19 @@ public class TimePickerDialog extends PreferenceDialogFragmentCompat {
 
     @Override
     public void onDialogClosed(boolean positiveResult) {
-
+        Log.d("CoffeeTime", "jedi");
+        minutes = Integer.valueOf(minuteDigit2.getText().toString() + minuteDigit1.getText().toString());
+        seconds = Integer.valueOf(secondDigit2.getText().toString() + secondDigit1.getText().toString());
+        Log.d("CoffeeTime", "Coffee Time is set to " + minutes + ":" + seconds);
     }
 
     @Override
     protected void onBindDialogView(View view) {
         super.onBindDialogView(view);
+        minuteDigit1 = view.findViewById(R.id.minute_digit_1);
+        minuteDigit2 = view.findViewById(R.id.minute_digit_2);
+        secondDigit1 = view.findViewById(R.id.second_digit_1);
+        secondDigit2 = view.findViewById(R.id.second_digit_2);
     }
-
 
 }
