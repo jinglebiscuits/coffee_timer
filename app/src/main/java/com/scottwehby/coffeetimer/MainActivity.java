fd496073eeb84824663e489ca19348ee968390bf
jinglebiscuits <scott.wehby@gmail.com>
2019-06-30 10:14:51 -0500

    added button to get to settings and back

--- app/src/main/java/com/scottwehby/coffeetimer/MainActivity.java
+++ app/src/main/java/com/scottwehby/coffeetimer/MainActivity.java
@@ -1,14 +1,11 @@
 package com.scottwehby.coffeetimer;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                  .setAction("Action", null).show();
-            }
-        });
     }
 
+    public void openSettings(View view) {
+        startActivity(new Intent(this, SettingsActivity.class));
+    }
 }
